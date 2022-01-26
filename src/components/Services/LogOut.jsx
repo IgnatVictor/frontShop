86bc24fcd82d5144f592c907c305cd3c531c9b0b
ignatvictor <meerr67@gmail.com>
2022-01-26 14:57:56 +0200

    fixed logged

--- src/components/Services/LogOut.jsx
+++ src/components/Services/LogOut.jsx
@@ -8,7 +8,7 @@ function LogOut() {
    localStorage.removeItem("id");
    localStorage.removeItem("quantity");
    
-   window.location.href = "https://webshopelectro.herokuapp.com"
+   window.location.href = "https://webshopelectro.herokuapp.com/api/products?page=0&limit=8"
 }
 
 export default LogOut;
