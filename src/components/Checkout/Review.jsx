2f5bc311cdefe8f2079a6cb2ff8826b1b5d7762e
ignatvictor <meerr67@gmail.com>
2022-03-10 03:27:44 +0200

    fix

--- src/components/Checkout/Review.jsx
+++ src/components/Checkout/Review.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Typography, List, ListItem, ListItemText } from "@material-ui/core";
 import { Products } from "..";
-import { useAtom } from "jotai";
-import {SUM} from "../atom/Atom"
+
 
 const Review = (props) => {
   const { cartItems } = props;
   let subtotal =0;
   
-  const [Sum,setSum] = useAtom(SUM)
+  
   
   
   
--- src/components/atom/Atom.jsx
+++ src/components/atom/Atom.jsx
@@ -4,3 +4,5 @@ export const LOGGEDIN = atom(false);
 export const MODALL = atom(false);
 export const CATEGORY = atom("");
 export const SEARCH = atom("");
+export const SUM= atom();
+export const PROFILE= atom();
